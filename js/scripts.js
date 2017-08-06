b2f9dca030a0815e9c7e101d0bc6fb1ea01e7b3d
Natalia <n.powalska@gmail.com>
2017-08-06 17:05:34 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+
+/*identifiers can be used as variable names, 
+and identifier names are valid unquoted property names*/
+var name = prompt('Enter your name');
+//alert('Hello, ' + name);
+console.log('Hello, ' + name);
+//linear comment
